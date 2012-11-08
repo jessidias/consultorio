d256a86925f815c37b5413f40791540a789f519e
631110001 <jessib.dias@gmail.com>
2012-11-08 09:03:47 -0200

    Apresentação do DAO ao professor. Classe agenda transformada em lista, para exibir mais de um registro, quando houver.

--- consultorio/src/AgendaDAO.java
+++ consultorio/src/AgendaDAO.java
@@ -4,11 +4,28 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Time;
+import java.util.ArrayList;
+import java.util.List;
+
+// TODO: fazer comentários 
+/**
+ * Classe AgendaDAO
+ * 
+ * @author jessib.dias@gmail.com
+ * 
+ */
 
 public class AgendaDAO {
 	private String selectFindAgenda = "select * from agenda where nome_paciente = ? and nome_medico = ?";
 
-	public Agenda findAgenda(String nome_paciente, String nome_medico) {
+	// TODO: acrescentar insert, update ou delete
+
+	/**
+	 * Foi adicionada uma lista à classe Agenda, para ela retornar mais de uma
+	 * resultado, se houver.
+	 */
+	public List<Agenda> findAgenda(String nome_paciente, String nome_medico) {
+
 		if (nome_paciente == null) {
 			throw new IllegalArgumentException(
 					"O nome do paciente não pode ser null.");
@@ -17,36 +34,39 @@ public class AgendaDAO {
 			throw new IllegalArgumentException(
 					"O nome do medico não pode ser null.");
 		}
-		Agenda c = null;
 
+		Agenda c = null;
+		List<Agenda> r = new ArrayList<Agenda>();
 		try {
 			Connection con = DriverManager.getConnection(
 					"jdbc:postgresql://localhost:5432/consultorio", "postgres",
 					"senacrs");
 
 			PreparedStatement stmt = con.prepareStatement(selectFindAgenda);
+			stmt.clearParameters();
 			stmt.setString(1, nome_paciente);
 			stmt.setString(2, nome_medico);
 			ResultSet rs = stmt.executeQuery();
-			if (rs.next()) {
+			while (rs.next()) {
 				String np = rs.getString("nome_paciente");
 				String nm = rs.getString("nome_medico");
 				int id_agenda = rs.getInt("id_agenda");
 				Date data_consulta = rs.getDate("data_consulta");
 				Time hora_consulta = rs.getTime("hora_consulta");
 				c = new Agenda(id_agenda, np, nm, data_consulta, hora_consulta);
+				r.add(c);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
-		return c;
+		return r;
 	}
 
 	public static void main(String[] args) {
 		AgendaDAO ag = new AgendaDAO();
-		Agenda c = ag.findAgenda("Jessica Dias", "Joao");
-		if (c == null) {
+		List<Agenda> c = ag.findAgenda("Jessica Dias", "Joao");
+		if (c.isEmpty()) {
 			System.out.println("Agenda não encontrada!");
 		} else {
 			System.out.println(c);
